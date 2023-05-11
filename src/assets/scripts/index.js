8a90d5dc610129c54f9471bdb3dad160803e1afc
Ryan Iug <ryaniugramos@gmail.com>
2023-05-11 11:09:13 -0300

    update

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -2,6 +2,13 @@ const tbody = document.querySelector(".tbody");
 const input = document.querySelector("#search-user");
 const button = document.querySelector(".button");
 
+input.addEventListener("keyup", key => {
+  if (key.code === "Enter") {
+    load();
+    input.value = "";
+  }
+});
+
 button.addEventListener("click", async () => {
   load();
   input.value = "";
@@ -10,6 +17,10 @@ button.addEventListener("click", async () => {
 async function load() {
   const value = input.value;
   const user = await apiGitHub.getUsers(value);
+  if (user.login === undefined) {
+    alert("User not found");
+    return;
+  }
   createTr(user);
   createBio(user);
 }
@@ -17,6 +28,9 @@ async function load() {
 function createTr(user) {
   const tr = document.createElement("tr");
   tr.classList.add("body-tr");
+
+  if (user.name === null || user.name === undefined) user.name = "";
+
   tr.innerHTML = `
   <td><img src="https://github.com/${user.login}.png" alt="User Image">
                             <a href="https://github.com/${user.login}" target="_blank">
